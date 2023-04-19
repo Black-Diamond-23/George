f4ab2bda69b048da9ddb91977dd41d3fb076f495
Zhaoju Liu <liu.zhaoj.northeastern.edu>
2023-04-19 12:14:27 +0800

    change configure and epatient

--- src/main/java/PatientManagement/Counfigure/ConfigurePM.java
+++ src/main/java/PatientManagement/Counfigure/ConfigurePM.java
@@ -1,5 +1,19 @@
 package PatientManagement.Counfigure;
 
+import javax.tools.DocumentationTool.Location;
+
+import PatientManagement.Catalogs.AgeGroup;
+import PatientManagement.Catalogs.VitalSignLimits;
+import PatientManagement.Catalogs.VitalSignsCatalog;
+import PatientManagement.Clinic.Clinic;
+import PatientManagement.Clinic.LocationList;
+import PatientManagement.Clinic.PatientDirectory;
+import PatientManagement.Clinic.Site;
+import PatientManagement.Clinic.SiteCatalog;
+import PatientManagement.Patient.Patient;
+import PatientManagement.Persona.Person;
+import PatientManagement.Persona.PersonDirectory;
+
 public class ConfigurePM {
     public ConfigurePM(int numClincs, int numPatients){
         
@@ -12,9 +26,62 @@ public class ConfigurePM {
         configureEPatient();
     }
 
-    private void configureClinics(){}
+    private void configureClinics(){
+        // Create clinics and give locations and sites
+        Clinic clinic = new Clinic("Kal's Clinic");
+        LocationList locations = clinic.getLocationList();
+        PatientManagement.Clinic.Location seattle  = locations.newLocation("Seattle area");
+        
+        SiteCatalog siteCatalog = clinic.getSiteCatalog();
+        Site CapitalHillCenter = siteCatalog.newSite(seattle);
+        // Can add more sites to the clinic.
 
-    private void configurePatients(){}
+        // configureVitalSigns();
+        VitalSignsCatalog vsc = clinic.getVitalSignsCatalog();
+        AgeGroup adults_21_50 = vsc.newAgeGroup("Adults 21-50", 50, 21);
+        VitalSignLimits heartRateLimits = vsc.newVitalSignLimits("HR");
+        VitalSignLimits bloodPressureLimits = vsc.newVitalSignLimits("BP");
+        VitalSignLimits bloodLipids = vsc.newVitalSignLimits("BL");
+
+        heartRateLimits.addLimits(adults_21_50, 105, 55);
+        bloodPressureLimits.addLimits(adults_21_50, 140, 70);
+        bloodLipids.addLimits(adults_21_50, 150, 100);
+
+        AgeGroup adults_51_90 = vsc.newAgeGroup("Adults 51-90", 90, 51);
+        heartRateLimits.addLimits(adults_51_90, 95, 40);
+        bloodPressureLimits.addLimits(adults_51_90, 120, 80);
+        bloodLipids.addLimits(adults_51_90, 200, 150);
+
+
+
+
+
+
+
+
+
+
+
+        
+
+
+        
+
+
+        
+
+    }
+
+    private void configurePatients(){
+        Clinic clinic = new Clinic("Kal's Clinic");
+        PersonDirectory pd = clinic.getPersonDirectory();
+        Person firstPerson = pd.newPerson("George", 18);
+        PatientDirectory patientDirectory = clinic.getPatientDirectory();
+        Patient archil = patientDirectory.newPatient(firstPerson);
+
+
+
+    }
 
     private void configureEPatient(){}
 }
--- src/main/java/PatitentManagement/EPatient/EPatient.java
+++ src/main/java/PatitentManagement/EPatient/EPatient.java
@@ -1,6 +1,7 @@
 package PatitentManagement.EPatient;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class EPatient {
     // Members
@@ -26,7 +27,21 @@ public class EPatient {
     }
 
     private void initMenu(){
+        welcomeMenu = new MenuEntry();
+        welcomeMenu.promptSetter("Hello, is this an emergency situation?");
+        welcomeMenu.optionsSetter(new ArrayList<String>(Arrays.asList("Yes, this is emergency!", "No, I'm fine.")));
 
+        emergencyMenu = new MenuEntry();
+        emergencyMenu.promptSetter("Ok, please call 911. Ending EPatient conversation. ");
+
+        MenuEntry normalMenu = new MenuEntry();
+        normalMenu.promptSetter("OK. I'm happy to help you but I haven't been inplemented.");
+        normalMenu.optionsSetter(new ArrayList<String>(Arrays.asList("Go back to main menu.")));
+        normalMenu.jumpToSetter(new ArrayList<MenuEntry>(Arrays.asList(welcomeMenu)));
+
+        welcomeMenu.jumpToSetter(new ArrayList<MenuEntry>(Arrays.asList(emergencyMenu, normalMenu)));
+
+        currentMenu = welcomeMenu;
     }
 
 }
